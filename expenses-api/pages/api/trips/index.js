b97170c7fc13ea5fb95d618ba532dfa30bf98935
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-04-16 23:32:13 -0700

    added a endpoint for an api

---
+++ expenses-api/pages/api/trips/index.js
@@ -0,0 +1,11 @@
+export default function handler(req, res) {
+  if (req.method !== "GET") {
+    //if not a get method send a message
+    res.status(405).json({
+      message: "Method Not Allowed",
+    });
+    return;
+  }
+
+  res.status(200).json({ test: "TRIPS!" });
+}
