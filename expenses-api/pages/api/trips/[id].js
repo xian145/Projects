47343ea82f15adef7aca443a75a7a98d0e2383dd
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-04-18 12:51:54 -0700

    added a method to post trips ussing insomnia in api.trips/index and a GET method for each trips in api/trips/[id].js

--- expenses-api/pages/api/trips/[id].js
+++ expenses-api/pages/api/trips/[id].js
@@ -8,6 +8,9 @@ export default async function handler(req, res) {
         id: parseInt(req.query.id), //req.query.id is a string so we use parseInt to transform that string to a integer, bc id need to be integer
       },
     });
+    if (!trip) {
+      return res.status(404).json({ error: "Trip not found" });
+    }
     res.status(200).json(trip);
   }
 
--- expenses-api/pages/api/trips/index.js
+++ expenses-api/pages/api/trips/index.js
@@ -1,11 +1,31 @@
 import prisma from "lib/prisma";
 
 export default async function handler(req, res) {
-  if (req.method !== "GET") {
-    res.status(405).send({ message: "Method Not Allowed" });
-    return;
+  if (req.method === "GET") {
+    //if get method just send back all the trips
+    const trips = await prisma.trip.findMany();
+    res.status(200).json(trips);
   }
 
-  const trips = await prisma.trip.findMany();
-  res.status(200).json(trips);
+  if (req.method === "POST") {
+    //if POST method accept it only if have a user and a name included, if not send an error
+    const { user, name, start_date, end_date } = req.body;
+
+    if (!user || !name) {
+      return res.status(400).json({ error: "user and name required" });
+    }
+
+    await prisma.trip.create({
+      //create the respective POST
+      data: {
+        user,
+        name,
+        start_date,
+        end_date,
+      },
+    });
+    return res.status(200).end();
+  }
+
+  res.status(405).json({ message: "Method Not Allowed" });
 }
