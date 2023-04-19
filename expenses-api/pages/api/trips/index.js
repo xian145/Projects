fee3e841ab5f70471379c6d99612747b39e3a726
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-04-18 22:44:05 -0700

    added a function to search for all expenses related to a trip, using Promise.all and .map() to create a new array and send that as a response

--- expenses-api/pages/api/trips/index.js
+++ expenses-api/pages/api/trips/index.js
@@ -4,6 +4,21 @@ export default async function handler(req, res) {
   if (req.method === "GET") {
     //if get method just send back all the trips
     const trips = await prisma.trip.findMany();
+
+    await Promise.all(
+      //as we have an .map method so we need to tell the program to wait for iterate all values that why we use promise.all()
+      trips.map(async (trip) => {
+        //trip is another variable that is actually an array created with .map() which contain the trip and the expense related to taht trip
+        trip.expenses = await prisma.expense.findMany({
+          //.expenses create a new key named "expenses" and the put the expenses related to that trip using .findMany
+          where: {
+            // find all expenses that have the id of the trip same as the .map() we create before
+            trip: trip.id,
+          },
+        });
+      })
+    );
+
     res.status(200).json(trips);
   }
 
