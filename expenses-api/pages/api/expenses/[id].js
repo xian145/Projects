4a7104ad0550ea26592b702913ab9626d7bd150d
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-04-19 11:10:49 -0700

    i donde really recall what i move here

--- expenses-api/pages/api/expenses/[id].js
+++ expenses-api/pages/api/expenses/[id].js
@@ -2,14 +2,17 @@ import prisma from "lib/prisma";
 
 export default async function handler(req, res) {
   if (req.method === "GET") {
-    const trip = await prisma.expense.findUnique({
+    const expense = await prisma.expense.findUnique({
       where: {
         id: parseInt(req.query.id), //req.query.id is a string so we use parseInt to transform that string to a integer, bc id need to be integer
       },
     });
-    if (!trip) {
+    if (!expense) {
       return res.status(404).json({ error: "Expense not found" });
     }
-    res.status(200).json(trip);
+    res.status(200).json(expense);
+  }
+
+  if (req.method === "PUT") {
   }
 }
