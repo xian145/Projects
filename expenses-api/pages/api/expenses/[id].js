1192e48851c0f4226448243e525662318defac45
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-04-19 12:13:07 -0700

    added a DELETE method in both expenses and trips index.js

--- expenses-api/pages/api/expenses/[id].js
+++ expenses-api/pages/api/expenses/[id].js
@@ -7,9 +7,11 @@ export default async function handler(req, res) {
         id: parseInt(req.query.id), //req.query.id is a string so we use parseInt to transform that string to a integer, bc id need to be integer
       },
     });
+
     if (!expense) {
       return res.status(404).json({ error: "Expense not found" });
     }
+
     res.status(200).json(expense);
   }
 
@@ -30,5 +32,15 @@ export default async function handler(req, res) {
     });
 
     return res.status(200).end("updated");
+  }
+
+  if (req.method === "DELETE") {
+    await prisma.expense.delete({
+      where: {
+        id: parseInt(req.query.id),
+      },
+    });
+
+    return res.status(200).end("Expense Deleted");
   }
 }
--- expenses-api/pages/api/trips/[id].js
+++ expenses-api/pages/api/trips/[id].js
@@ -8,9 +8,11 @@ export default async function handler(req, res) {
         id: parseInt(req.query.id), //req.query.id is a string so we use parseInt to transform that string to a integer, bc id need to be integer
       },
     });
+
     if (!trip) {
       return res.status(404).json({ error: "Trip not found" });
     }
+
     res.status(200).json(trip);
   }
 
@@ -33,5 +35,12 @@ export default async function handler(req, res) {
   }
 
   if (req.method === "DELETE") {
+    await prisma.trip.delete({
+      where: {
+        id: parseInt(req.query.id),
+      },
+    });
+
+    return res.status(200).end("Trip Deleted");
   }
 }
