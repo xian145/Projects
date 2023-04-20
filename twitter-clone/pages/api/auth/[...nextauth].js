de7501b563bd8fb46f7c24c81aac6a525b586230
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-04-19 21:24:58 -0700

    all needed for authorization from email using mailtrap

---
+++ twitter-clone/pages/api/auth/[...nextauth].js
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "lib/prisma";
+
+export const authOptions = {
+  providers: [
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
+
+  database: process.env.DATABASE_URL,
+  secret: process.env.SECRET,
+
+  session: {
+    jwt: true,
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
+
+  adapter: PrismaAdapter(prisma),
+
+  callbacks: {
+    session: async ({ session, user }) => {
+      session.user.id = user.id;
+      session.user.username = user.username;
+      return Promise.resolve(session);
+    },
+  },
+};
+
+export default NextAuth(authOptions);
--- twitter-clone/pages/index.js
+++ twitter-clone/pages/index.js
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div>
       <h1>Home page</h1>
+      <Link to="/api/auth/signin">Login</Link>
     </div>
   );
 }
