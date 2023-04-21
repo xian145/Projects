c7b825a6e80df516fc968bbf946829706b6d862f
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-04-20 22:36:47 -0700

    added style to the text area and button to look like twitter

--- twitter-clone/components/newTweet.js
+++ twitter-clone/components/newTweet.js
@@ -8,6 +8,13 @@ export default function NewTweet() {
   //we will not redirect since this is a component, simply will not render.
   if (!session || !session.user) return null;
 
+  const handleInput = (event) => {
+    const element = event.target;
+    element.style.height = "auto";
+    element.style.height = `${element.scrollHeight}px`;
+    setContent(event.target.value);
+  };
+
   return (
     <form
       onSubmit={async (e) => {
@@ -21,25 +28,29 @@ export default function NewTweet() {
         alert(content);
       }}
     >
-      <div className="flex">
-        <div className="flex px-1 pt-2 mt-2 ml-1 mr-1">
-          <textarea
-            className="w-full p-4 text-lg font-medium bg-transparent border outline-none color-primary"
-            row={2}
-            column={50}
-            placeholder="What's happening?"
-            name="content"
-            onChange={(e) => setContent(e.target.value)}
-          ></textarea>
+      <div>
+        <div className="flex items-center justify-center w-full">
+          <div className="flex w-1/3 px-1 pt-2 mt-2 ml-1 mr-1">
+            <textarea
+              className="w-full p-4 overflow-hidden text-lg font-medium bg-transparent border outline-none resize-none"
+              maxLength={280}
+              placeholder="What's happening?"
+              name="content"
+              onChange={handleInput}
+            ></textarea>
+          </div>
+        </div>
+        <div className="flex justify-center w-full">
+          <div className="flex justify-end w-1/3 px-1 pt-2 mt-2 ml-1 mr-1">
+            <button className="flex px-8 py-2 mt-0 mr-2 font-bold border rounded-full item-end bg-[#1da1f2]">
+              Tweet
+            </button>
+          </div>
         </div>
       </div>
 
-      <div>
-        <div>
-          <button className="text-black bg-blue-300 border hover:bg-blue-600">
-            Tweet
-          </button>
-        </div>
+      <div className="flex">
+        <div className="flex-1 mb-5"></div>
       </div>
     </form>
   );
