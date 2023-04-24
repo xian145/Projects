c1a9eb38f1fbb36df7f217f69395ca3735cd1ce7
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-04-24 12:54:28 -0700

    changes in api/tweet

--- twitter-clone/pages/api/tweet.js
+++ twitter-clone/pages/api/tweet.js
@@ -1,4 +1,20 @@
+import prisma from "@/lib/prisma";
+import { AuthOptions } from "pages/api/auth/[...nextauth].js";
+import { getServerSession } from "next-auth";
+
 export default async function handler(req, res) {
+  const session = await getServerSession(req, res, AuthOptions);
+
+  if (!session) return res.status(401).json({ message: "Not logged in" });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  });
+
+  if (!user) return res.status(401).json({ message: "user not found" });
+
   //this function will handle the request if is a POST request
   if (req.method === "POST") {
     console.log(req.body);
