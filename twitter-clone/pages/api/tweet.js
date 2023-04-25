3510ca1d17d8e0315fd2ed88479d4faf275a0c40
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-04-24 18:05:12 -0700

    added comments to understand what happens and fix the a problem with a function that was written with capital A when should be simple a

--- twitter-clone/pages/api/tweet.js
+++ twitter-clone/pages/api/tweet.js
@@ -1,18 +1,21 @@
 import prisma from "@/lib/prisma";
-import { AuthOptions } from "pages/api/auth/[...nextauth].js";
 import { getServerSession } from "next-auth";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function handler(req, res) {
-  const session = await getServerSession(req, res, AuthOptions);
+  const session = await getServerSession(req, res, authOptions);
 
+  //chek if the session is active
   if (!session) return res.status(401).json({ message: "Not logged in" });
 
+  //search for the user in the db
   const user = await prisma.user.findUnique({
     where: {
       email: session.user.email,
     },
   });
 
+  //if user fetched is not succesful
   if (!user) return res.status(401).json({ message: "user not found" });
 
   //this function will handle the request if is a POST request
