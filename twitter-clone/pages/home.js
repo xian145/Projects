fc4dd4437320629404799c3dc4edd4abb161dc46
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-04-24 23:03:07 -0700

    added the component Tweet to the homepage and send some information to know if its working

--- twitter-clone/pages/home.js
+++ twitter-clone/pages/home.js
@@ -1,28 +1,39 @@
 import { useSession } from "next-auth/react"; //to check if the user is logged in
 import { useRouter } from "next/router";
 import NewTweet from "@/components/newTweet";
+import Tweets from "@/components/tweets";
 
 export default function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
 
   if (status === "loading") {
-    return <p>Loggin</p>;
+    //render nothing if loading
+    return null;
   }
 
   if (!session) {
+    //return to root page if not loggin
     router.push("/");
   }
 
   return (
-    <div>
+    <>
       <NewTweet />
+      <Tweets
+        tweets={[
+          { content: "test" },
+          { content: "another" },
+          { content: "me la pelas", pelas: "si pelas" },
+        ]}
+      />
+      {/*we call to the component Tweets and send some information (tweets)*/}
       <a
         href="/api/auth/signout"
         className="border border-gray-100 hover:bg-red-600"
       >
         Logout
       </a>
-    </div>
+    </>
   );
 }
