a401c39315f49fddb02bf65e3ee4d00c9c3856b3
Abraham Ariel Arizmendi Grijalva <70303807+xian145@users.noreply.github.com>
2023-04-25 22:36:45 -0700

    added som style to the tweet

--- twitter-clone/components/tweet.js
+++ twitter-clone/components/tweet.js
@@ -4,10 +4,16 @@ export default function Tweet({ tweet }) {
   //recive as an argument tweet, that came from tweets that came form home, and return a <p></p> with the content of the tweet
   return (
     //content is a property that came from the array
-    <p>
-      {tweet.author.email}
-      {timeago.format(new Date(tweet.createdAt))}
-      {tweet.content}
-    </p>
+    <div className="flex flex-col p-3 m-3 border border-solid">
+      <div className="flex space-x-2">
+        <div className="font-extrabold ">{tweet.author.name}</div>
+        <div className="text-gray-400">@{tweet.author.username}</div>
+        <div className="text-gray-400">·</div>
+        <div className="text-gray-400">
+          {timeago.format(new Date(tweet.createdAt))}
+        </div>
+      </div>
+      <div className="tweet-content">{tweet.content}</div>
+    </div>
   );
 }
