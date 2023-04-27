b69e6e1108f512547e26ea5b3649e5ad19f11053
xian145 <abraham.arizmendi.grijalva@gmail.com>
2023-04-26 17:47:16 -0700

    utils change

--- twitter-clone/pages/utils.js
+++ twitter-clone/pages/utils.js
@@ -1,11 +1,11 @@
 export default function Utils() {
   return (
-    <div className="mt-10 ml-20">
-      <h2 className="mb-10 text-xl">Utils</h2>
+    <div className="flex flex-col w-1/2 p-4 mx-auto border">
+      <h2 className="mx-auto text-xl">Utils</h2>
 
-      <div className="flex-1 mb-5">
+      <div className="flex-1 mx-auto">
         <button
-          className="px-8 py-2 mt-5 mr-8 font-bold border rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker"
+          className="px-8 py-2 mt-5 mr-8 font-bold border rounded-full hover:bg-gray-400 color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker"
           onClick={async () => {
             await fetch("/api/utils", {
               body: JSON.stringify({
@@ -21,9 +21,9 @@ export default function Utils() {
           Clean database
         </button>
       </div>
-      <div className="flex-1 mb-5">
+      <div className="flex-1 mx-auto">
         <button
-          className="px-8 py-2 mt-5 mr-8 font-bold border rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker"
+          className="px-8 py-2 mt-5 mr-8 font-bold border rounded-full hover:bg-gray-400 color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker"
           onClick={async () => {
             await fetch("/api/utils", {
               body: JSON.stringify({
@@ -39,9 +39,9 @@ export default function Utils() {
           Generate users and tweets
         </button>
       </div>
-      <div className="flex-1 mb-5">
+      <div className="flex-1 mx-auto">
         <button
-          className="px-8 py-2 mt-5 mr-8 font-bold border rounded-full color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker"
+          className="px-8 py-2 mt-5 mr-8 font-bold border rounded-full hover:bg-gray-400 color-accent-contrast bg-color-accent hover:bg-color-accent-hover-darker"
           onClick={async () => {
             await fetch("/api/utils", {
               body: JSON.stringify({
